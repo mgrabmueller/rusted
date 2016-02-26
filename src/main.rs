c11590164c17f40edd794e2d8cd93ac7eee4c6b7
Martin Grabmueller <martin@grabmueller.de>
2016-02-26 17:24:53 +0100

    Insertion from iterators, Error implementation, various tweaks.

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,11 @@
 //! - "Moving the gap" means moving the buffer content so that gaplen
 //!   bytes following the new gap position are unused.
 
+use std::io;
+use std::string::FromUtf8Error;
+use std::error;
+use std::fmt;
+
 /// Allowed encodings for text content.
 ///
 /// Currently, only UTF-8 is supported, because this is the standard
@@ -57,11 +62,53 @@ pub enum Mode {
 }
 
 /// Errors produced by this module.
-#[derive(Debug,Clone,Copy,PartialEq,Eq)]
+#[derive(Debug)]
 pub enum Error {
     /// This error is returned whenever an operation is performed that
     /// is only meaningful for buffers in text mode.
-    NotInTextMode
+    NotInTextMode,
+    NotUTF8,
+    IoError(io::Error)
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+impl From<FromUtf8Error> for Error {
+    fn from(_: FromUtf8Error) -> Error {
+        Error::NotUTF8
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::NotInTextMode => write!(f, "buffer not in text mode"),
+            Error::NotUTF8 => write!(f, "not in UTF-8"),
+            Error::IoError(ref err) => write!(f, "IO error: {}", err)
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::NotInTextMode => "buffer not in text mode",
+            Error::NotUTF8 => "not in UTF-8",
+            Error::IoError(ref err) => err.description()
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::NotUTF8 => None,
+            Error::NotInTextMode => None,
+            Error::IoError(ref err) => Some(err)
+        }
+    }
 }
 
 /// Gap buffer implementation.
@@ -76,6 +123,8 @@ pub struct Buf {
     /// Length of the gap.
     gaplen: usize
 }
+
+const DEFAULT_GAPLEN: usize = 128;
 
 impl Buf {
     /// Create a new empty buffer with the provided mode.
@@ -194,6 +243,20 @@ impl Buf {
         self.gaplen -= l;
     }
 
+    pub fn insert_all<I>(&mut self, it: I, pos: usize)
+        where I: Iterator<Item=u8> {
+        let mut i = pos;
+        for b in it {
+            if self.gaplen < DEFAULT_GAPLEN {
+                self.mkgap(i, DEFAULT_GAPLEN);
+            }
+            self.buf[i] = b;
+            i += 1;
+            self.gap += 1;
+            self.gaplen -= 1;
+        }
+    }
+
     /// Delete `cnt` bytes at position `at`.
     ///
     /// # Panics
@@ -249,7 +312,6 @@ impl Buf {
     fn movegap(&mut self, ngap: usize) {
         if ngap < self.gap {
             let movecnt = self.gap - ngap;
-            println!("gap: {}, ngap: {}", self.gap, ngap);
             for i in 0..movecnt {
                 self.buf[self.gap + self.gaplen - 1 - i] = self.buf[self.gap - i - 1];
             }
@@ -288,7 +350,10 @@ impl Buf {
                 let mut v = Vec::new();
                 v.extend(&self.buf[0..self.gap]);
                 v.extend(&self.buf[self.gap+self.gaplen..self.buf.len()]);
-                Ok(String::from_utf8(v).unwrap())
+                match String::from_utf8(v) {
+                    Ok(s) => Ok(s),
+                    Err(_) => Err(Error::NotUTF8)
+                }
             }
             _ => Err(Error::NotInTextMode)
         }
@@ -348,11 +413,11 @@ impl<'a> Iterator for Bytes<'a> {
 
 fn main() {
     let mut buf2 = Buf::from_str("Hällo!\n", LineEnding::Lf);
-    println!("{:?} {}", buf2, buf2.len());
+    println!("{:#?} {}", buf2, buf2.len());
     println!("{}", buf2.to_string().unwrap());
 
     buf2.insert_str("NEW", 1);
-    println!("{:?} {}", buf2, buf2.len());
+    println!("{:#?} {}", buf2, buf2.len());
     println!("{}", buf2.to_string().unwrap());
 }
 
@@ -364,7 +429,7 @@ mod test {
         let buf = Buf::new(mode);
         assert_eq!(buf.len(), 0);
         assert_eq!(buf.mode, mode);
-        assert_eq!(buf.to_string(), Ok("".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "".to_string());
     }
 
     #[test]
@@ -374,16 +439,16 @@ mod test {
         let buf = Buf::from_str("Überfjäll", le);
         assert_eq!(buf.len(), 11);
         assert_eq!(buf.mode, Mode::Text(Encoding::UTF8, le));
-        assert_eq!(buf.to_string(), Ok("Überfjäll".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "Überfjäll".to_string());
     }
 
     #[test]
     fn with_capacity() {
-        use super::{Mode, Buf, Error};
+        use super::{Mode, Buf};
         let buf = Buf::with_capacity(32, Mode::Binary);
         assert_eq!(buf.len(), 0);
         assert_eq!(buf.mode, Mode::Binary);
-        assert_eq!(buf.to_string(), Err(Error::NotInTextMode));
+        assert!(buf.to_string().is_err());
     }
 
     #[test]
@@ -407,16 +472,32 @@ mod test {
 
         buf.insert_str("tjord", 5);
         assert_eq!(buf.len(), 16);
-        assert_eq!(buf.to_string(), Ok("Übertjordfjäll".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "Übertjordfjäll".to_string());
 
         buf.insert_str("bJoll", 0);
         assert_eq!(buf.len(), 21);
-        assert_eq!(buf.to_string(), Ok("bJollÜbertjordfjäll".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "bJollÜbertjordfjäll".to_string());
 
         let l = buf.len();
         buf.insert_str("Twaanrö", l);
         assert_eq!(buf.len(), 29);
-        assert_eq!(buf.to_string(), Ok("bJollÜbertjordfjällTwaanrö".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "bJollÜbertjordfjällTwaanrö".to_string());
+    }
+
+    #[test]
+    fn insert_all() {
+        use super::{Mode, Encoding, LineEnding, Buf};
+        let mode = Mode::Text(Encoding::UTF8, LineEnding::Lf);
+        let mut buf = Buf::new(mode);
+
+        buf.insert_all("Hallo ällerseits!".to_string().bytes(), 0);
+        assert_eq!(buf.to_string().unwrap(), "Hallo ällerseits!".to_string());
+
+        buf.insert_all("auch ".to_string().bytes(), 6);
+        assert_eq!(buf.to_string().unwrap(), "Hallo auch ällerseits!".to_string());
+
+        buf.insert_all("o_O".to_string().bytes(), 23);
+        assert_eq!(buf.to_string().unwrap(), "Hallo auch ällerseits!o_O".to_string());
     }
 
     #[test]
@@ -428,15 +509,15 @@ mod test {
 
         buf.delete(7, 3);
         assert_eq!(buf.len(), 8);
-        assert_eq!(buf.to_string(), Ok("Überfjl".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "Überfjl".to_string());
 
         buf.delete(0, 2);
         assert_eq!(buf.len(), 6);
-        assert_eq!(buf.to_string(), Ok("berfjl".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "berfjl".to_string());
 
         let p = buf.len() - 1;
         buf.delete(p, 1);
         assert_eq!(buf.len(), 5);
-        assert_eq!(buf.to_string(), Ok("berfj".to_string()));
+        assert_eq!(buf.to_string().unwrap(), "berfj".to_string());
     }
 }
